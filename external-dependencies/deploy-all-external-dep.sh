2de344c87a1916a4ed30fa68addba0b3378af37f
Joao Lopes <lopes.joao.ca@gmail.com>
2025-06-06 00:14:42 +0100

    Updated versions and removed mongo db.

--- external-dependencies/deploy-all-external-dep.sh
+++ external-dependencies/deploy-all-external-dep.sh
@@ -1,6 +1,5 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
-helm install mongodb -f mongo-db/values.yaml bitnami/mongodb
 chmod +x cloudflare-tunnel/deploy_tunnel.sh
 cloudflare-tunnel/deploy_tunnel.sh
 helm install postgres -f postgres-db/values.yaml oci://registry-1.docker.io/bitnamicharts/postgresql --namespace magic-key
